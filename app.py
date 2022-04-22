14cc3eda59bf79f2ba92bce993efe58647a84de6
Randomness74 <43918046+Randomness74@users.noreply.github.com>
2022-04-22 09:10:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 from datetime import timedelta, datetime
 
 # connect to database ( MongoDB )
-client = pymongo.MongoClient("mongodb+srv://Rand74:Carsfrom1988!@gridcluster.1zpdx.mongodb.net/GridCluster?retryWrites=true&w=majority")
-# client = pymongo.MongoClient("mongodb+srv://doadmin:c70T1G4xR9hD586r@gridcluster.1zpdx.mongodb.net/GridCluster?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://doadmin:c70T1G4xR9hD586r@gridcluster.1zpdx.mongodb.net/GridCluster?retryWrites=true&w=majority")
 db = client["GridCluster"]
 col = db["credentials"]
 boxData = db["boxdata"]
